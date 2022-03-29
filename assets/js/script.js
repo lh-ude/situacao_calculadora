7f7667e95f3de9d051c8c7bb461b6e547ec43c6a
Luíza Araújo <99282366+lh-ude@users.noreply.github.com>
2022-03-28 21:56:55 -0300

    update 28-03-2021

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,85 @@ const reset = function(){
     labelVisor.innerHTML= visor 
 }
 
+const clickn0 = function(){
+    visor = visor + '0'
+    labelVisor.innerHTML= visor 
+}
+
+const clickn1 = function(){
+    visor = visor + '1'
+    labelVisor.innerHTML= visor 
+}
+
+const clickn2 = function(){
+    visor = visor + '2'
+    labelVisor.innerHTML= visor 
+}
+
+const clickn3 = function(){
+    visor = visor + '3'
+    labelVisor.innerHTML= visor 
+}
+
+const clickn4 = function(){
+    visor = visor + '4'
+    labelVisor.innerHTML= visor 
+}
+const clickn5 = function(){
+    visor = visor + '5'
+    labelVisor.innerHTML= visor 
+}
+const clickn6 = function(){
+    visor = visor + '6'
+    labelVisor.innerHTML= visor 
+}
+
 const clickn7 = function(){
     visor = visor + '7'
     labelVisor.innerHTML= visor 
 }
-
 const clickn8 = function(){
     visor = visor + '8'
     labelVisor.innerHTML= visor 
-    
-}
+    }
 
 const clickn9 = function(){
     visor = visor + '9'
     labelVisor.innerHTML= visor     
+}
+const clickOpSom = function(){
+    operacao = 'adicao'   
+    let numero = parseFloat(visor)
+    valor1 = numero
+    valor2 = numero
+}
+
+const clickOpSub = function(){
+    operacao = 'subtracao'   
+    let numero = parseFloat(visor)
+    valor1 = numero
+    valor2 = numero
+}
+
+const clickOpMult = function(){
+    operacao = 'multiplicacao'   
+    let numero = parseFloat(visor)
+    valor1 = numero
+    valor2 = numero
 }
 
 const clickOpDiv = function(){
     operacao = 'divisao'   
     let numero = parseFloat(visor)
     valor1 = numero
+    valor2 = numero
 }
 
 const igual = function () {
     
     switch(operacao){
-        case 'divisao':
+        case 'adicao':
             
-        break
+            
     }
 }
